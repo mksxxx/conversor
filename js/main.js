63ac46dd69be71ce7a1d353bc2890457002776de
Marcos Vinicius <marcos.10823460@gmail.com>
2025-01-07 19:44:44 -0300

    Adicionando kelvin

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 function calc(){
     let tmp1 = document.getElementById('tmp1').value;
-    let convert = tmp1 * 9/5 + 32;
+    let c1 = tmp1 * 9/5 + 32;
+    let c2 = tmp1 + 273.15;
 
-    document.getElementById('resultado').value = convert
+    document.getElementById('resultado').value = c1
+    document.getElementById('resultado').value = c2
 
     console.log(tmp1);
 }
